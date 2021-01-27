cf44c2a37922605c79163b3b1bb3b31ffa504362
Kartavya kothari <kartavya.kothari24@gmail.com>
2021-01-27 11:02:12 +0530

    Lecture 1

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,22 +5,48 @@ using namespace std;
 // public
 // protected
 
+struct Node{
+    int val;
+    char gender;
+};
+
+union Node{
+    int val;
+    int gender;
+    char dog;
+};
+
 class Person{
     int age;
     string name;
 
     public:
+    static string species;
+
     Person(string n, int a){
         name = n;
         age = a;
     }
 
     void print_details(){
-        cout<<"Hi, "<<name<<", you are "<<age<<" years old!";
+        cout<<"Hi "<<species<<", "<<name<<", you are "<<age<<" years old!";
     }
 };
 
+void cnt_up(){
+    // auto cnt = 0;
+    static int cnt = 0;
+    cnt++;
+    cout<<cnt<<endl;
+}
+
+string Person::species = "human";
+
 int main(){
+    // cnt_up();
+    // cnt_up();
+    // cnt_up();
+
     int age;
     string name;
 
@@ -31,8 +57,10 @@ int main(){
     cin>>age;
 
     Person *p = new Person(name,age);
+    Person *p1 = new Person("Nitin",20);
 
     p->print_details();
+    p1->print_details();
 
     return 0;
 }
