63183b0a7712f4eb67e528279b5cb52bf8e747c7
Kartavya kothari <kartavya.kothari24@gmail.com>
2021-01-30 11:01:28 +0530

    Added all codes

--- templates.cpp
+++ templates.cpp
@@ -14,6 +14,11 @@ void print_this(T val){
 //     cout<<"Variable type = "<<typeid(val).name()<<" and Value is "<<val<<endl;
 // }
 
+// #define pythagous(a,b) sqrt(a*a+b*b)
+inline double pythagorus(double x, double y){
+    return sqrt(x*x+y*y);
+}
+
 int main(){
     int p = 10;
     double q = 10.3;
