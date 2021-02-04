eb1f5218871d22fb75f1df7feef3444958cf1e3a
Kartavya kothari <kartavya.kothari24@gmail.com>
2021-02-04 08:57:19 +0530

    Added more features

--- bank.cpp
+++ bank.cpp
@@ -5,10 +5,17 @@ using namespace std;
 // public
 // protected 
 
+struct transaction{
+    bool is_deposit;
+    int amt;
+};
+
 class Account{
     int acc_num;
     int balance;
     string pass;
+    struct transaction transactions[100];
+    int last_in;
 
     bool check_pass(string p){
         if(p==pass){
@@ -24,6 +31,7 @@ class Account{
         acc_num = an;
         balance = bal;
         pass = p;
+        last_in = -1;
     }
 
     void check_balance(string p){
@@ -39,6 +47,9 @@ class Account{
                 return;
             }
 
+            last_in++;
+            transactions[last_in] = {false,amt};
+
             balance = balance-amt;
             cout<<"Withdrawing "<<amt<<" rupess. ";
             check_balance(p);
@@ -47,10 +58,27 @@ class Account{
 
     void deposit_money(string p, int amt){
         if(check_pass(p)){
+            last_in++;
+            transactions[last_in] = {true,amt};
             balance = balance+amt;
             cout<<"Adding "<<amt<<" rupess. ";
             check_balance(p);
         }
+    }
+
+    void print_transaction_history(string p){
+        if(!check_pass(p))
+            return;
+        
+        cout<<endl<<"*** Printing transaction history ***"<<endl;
+
+        for(int i = 0 ; i <= last_in ; i++){
+            if(transactions[i].is_deposit){
+                cout<<"Deposited "<<transactions[i].amt<<" rupees"<<endl;
+            }else cout<<"Withdrew "<<transactions[i].amt<<" rupees"<<endl;
+        }
+
+        cout<<"***        ***"<<endl<<endl;
     }
 };
 
@@ -64,12 +92,22 @@ int main(){
     // Transiction history
     // Transfer money
 
-    Account p1(1,100,"pas@123");
-    p1.check_balance("pas@123");
-    p1.deposit_money("pas@123",500);
-    p1.withdraw_money("pas@123",1000);
-    p1.deposit_money("pas@123",500);
-    p1.withdraw_money("pas@123",1000);
+    // Account p1(1,100,"pas@123");
+    // p1.check_balance("pas@123");
+    // p1.deposit_money("pas@123",500);
+    // p1.withdraw_money("pas@123",1000);
+    // p1.deposit_money("pas@123",500);
+    // p1.withdraw_money("pas@123",1000);
+    // p1.print_transaction_history("pas@123");
+
+    Account *p1 = new Account(1,100,"pas@123");
+    
+    p1->check_balance("pas@123");
+    p1->deposit_money("pas@123",500);
+    p1->withdraw_money("pas@123",1000);
+    p1->deposit_money("pas@123",500);
+    p1->withdraw_money("pas@123",1000);
+    p1->print_transaction_history("pas@123");
 
     return 0;
 }
